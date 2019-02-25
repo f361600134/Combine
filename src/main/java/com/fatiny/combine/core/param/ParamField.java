949bdfe781ea3333f841de2d4e7abe82c3f07bb6
Administrator <Administrator@192.168.0.106>
2019-02-25 23:29:09 +0800

    aaaaaaaaaaa

--- src/main/java/com/fatiny/combine/core/param/ParamField.java
+++ src/main/java/com/fatiny/combine/core/param/ParamField.java
@@ -1,5 +1,8 @@
 package com.fatiny.combine.core.param;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParamField {
 
 	private String prefix;
@@ -8,6 +11,9 @@ public class ParamField {
 	private String field;
 	private String value;
 
+	// Key:filed, value:propertiy.value
+	private Map<String, String> field2value;
+
 	public ParamField(String prefix, String mainWord, String keyIndex, String field, String value) {
 		super();
 		this.prefix = prefix;
@@ -15,14 +21,23 @@ public class ParamField {
 		this.keyIndex = keyIndex;
 		this.field = field;
 		this.value = value;
+		this.field2value = new HashMap<String, String>();
 	}
 
-	public ParamField(String mainWord, String keyIndex, String field, String value) {
+	// public ParamField(String mainWord, String keyIndex, String field, String
+	// value) {
+	// super();
+	// this.mainWord = mainWord;
+	// this.keyIndex = keyIndex;
+	// this.field = field;
+	// this.value = value;
+	// }
+
+	public ParamField(String mainWord, String keyIndex, Map<String, String> field2value) {
 		super();
 		this.mainWord = mainWord;
 		this.keyIndex = keyIndex;
-		this.field = field;
-		this.value = value;
+		this.field2value = field2value;
 	}
 
 	public String getPrefix() {
